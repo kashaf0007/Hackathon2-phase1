247ab25ffe7e331ac446525e0e39ab420996e63c
Kashaf Aman <166901772+kashaf0007@users.noreply.github.com>
2026-01-08 22:48:03 +0500

    updated

--- Todo-app-phase2/frontend/next.config.js
+++ Todo-app-phase2/frontend/next.config.js
@@ -9,14 +9,6 @@ const nextConfig = {
       enabled: false
     }
   },
-  webpack: (config, { isServer }) => {
-    // Ensure that the '@' alias is properly resolved by webpack
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': __dirname + '/src',
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
